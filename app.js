b1a20526d006c9dda287febfbc3084c70a073c5e
Hannah Baumwell <hpbaumwell@gmail.com>
2017-11-02 18:42:59 -0400

    Almost finished

---
+++ app.js
@@ -0,0 +1,47 @@
+function onReady () {
+  const addToDoForm = document.getElementById('addToDoForm');
+  const newToDoText = document.getElementById('newToDoText');
+  const toDoList = document.getElementById('toDoList');
+  addToDoForm.addEventListener ('submit', event => {
+    event.preventDefault();
+    // get the text
+    let title = newToDoText.value;
+
+    // create a new li
+    let newLi = document.createElement('li');
+
+    // create a new input
+    let checkbox = document.createElement('input');
+
+    // set the input's type to  checkbox
+    checkbox.type = "checkbox";
+
+    // set the title
+    newLi.textContent = title;
+
+    // attach the checkbox to the LI
+    newLi.appendChild(checkbox);
+
+    // attach the li to the ul
+    toDoList.appendChild(newLi);
+
+    //empty the input
+    newToDoText.value = '';
+
+    //create deletebutton
+    let deleteButton = document.createElement('submit');
+
+    // set it to be a button
+    deleteButton.type = "submit";
+
+    // attach the delete button
+    newLi.appendChild(deleteButton);
+
+
+
+  });
+}
+
+window.onload = function (){
+  onReady();
+}
